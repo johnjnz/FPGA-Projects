e3ec0729f463fbf44a836f8f28f2f637d1036c32
johnjnz <john@johnston.email>
2014-10-10 18:39:11 +1300

    Initial Commit

---
+++ Simulations/light/lightVHD.vhd
@@ -0,0 +1,39 @@
+
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity lightVHD is
+end lightVHD;
+
+architecture behav of lightVHD is
+  
+  component light
+    port (
+      x1 :in std_logic;
+      x2 :in std_logic;
+      f :out std_logic);
+  end component;
+  
+  signal ain :std_logic :='0';
+  signal bin :std_logic :='0';
+  signal cout :std_logic;
+  begin
+    
+    U1 :light port map(x1 => ain, x2=> bin, f=> cout);
+    
+    process
+    begin
+      ain <= '0';        
+      wait for 20 ns;
+      ain <= '1';
+      wait for 20 ns;
+    end process;
+      
+    process
+    begin
+      bin <= '0';
+      wait for 40 ns;
+      bin <= '1';
+      wait for 20 ns;
+    end process;
+end behav;
